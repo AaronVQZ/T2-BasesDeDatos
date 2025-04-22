031af4e6dea9f65c5294daa35266e2582588b2a3
AaronVQZ <av.vasquezaaron@gmail.com>
2025-04-21 21:12:00 -0600

    mostrar empleados

--- empleados/views.py
+++ empleados/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.db import connection
 def home(request):
-    return render(request, "home.html")
+
+    lista_empleados = obtener_empleados()
+
+    return render(request, "home.html", {"empleados": lista_empleados})
+
+
+def obtener_empleados():
+    
+    query = "EXEC sp_obtenerEmpleados"
+    connection.ensure_connection()
+    conn = connection.connection
+    empleados = conn.execute(query).fetchall()
+    return empleados
+    
 
